eff284fd52536c64af9d6e6931542ae01a4129b6
Orçun Odabaşı <orcunodabasi@sunrise.ch>
2012-07-18 18:37:00 +0200

    initial blog model post created

--- webtech/blog/models.py
+++ webtech/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField("Title", max_length=100)
+    content = models.TextField()
+    publish_date = models.DateTimeField(auto_now=True)
+    tags = TaggableManager()
+    
+    
